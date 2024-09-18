5f6ddf9cc0f7bb61e6b770f55b2da4ec32d996b1
sbakhareva <snezhana.bakhareva@yandex.ru>
2024-09-18 15:29:54 +0300

    готовое дз (98% уверенности)

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ public class Main {
         int clientOS = 1;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
-        }
-        if (clientOS == 1) {
+        } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке.");
         }
 
@@ -49,50 +48,48 @@ public class Main {
         }
 
 
-
         // Task #3
         // високосным является каждый четвертый И каждый четырехсотый год, каждый сотый — не високосный
-        int year = 2016;
-        if (year > 1584) {
-            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-                System.out.println(year + " год является високоснымю");
-            } else {
+        int year = 2020;
+        if (year < 1584) {
+            System.out.println("Год не может быть меньше 1584.");
+        } else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високоснымю");
+        } else {
                 System.out.println(year + " год не является високосным.");
             }
-        } else {
-                System.out.println("Год не может быть меньше 1585.");
+
+
+            // Task #4
+            int deliveryDistance = 53;
+            int deliveryDays = 1;
+            if (deliveryDistance <= 20) {
+                System.out.println("Потребуется дней: " + (deliveryDays));
+            } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+                System.out.println("Потребуется дней: " + (deliveryDays + 1));
+            } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+                System.out.println("Потребуется дней: " + (deliveryDays + 2));
+            } else {
+                System.out.println("В этом радиусе доставки нет.");
             }
 
-        // Task #4
-        int deliveryDistance = 95;
-        int deliveryDays = 1;
-        if (deliveryDistance <= 20) {
-            System.out.println("Потребуется дней: " + (deliveryDays));
-        } if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней: " + (deliveryDays + 1));
-        } if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней: " + (deliveryDays +2));
-        } else if (deliveryDistance > 100) {
-            System.out.println("В этом радиусе доставки нет.");
-        }
-
-        // Task #5
-        int monthNumber = 7;
-        switch (monthNumber) {
-            case 12, 1, 2:
-                System.out.println("Время года - зима.");
-                break;
-            case 3, 4, 5:
-                System.out.println("Время года - весна.");
-                break;
-            case 6, 7, 8:
-                System.out.println("Время года - лето.");
-                break;
-            case 9, 10, 11:
-                System.out.println("Время года - осень.");
-                break;
-            default:
-                System.out.println("Такого месяца не существует.");
+            // Task #5
+            int monthNumber = 7;
+            switch (monthNumber) {
+                case 12, 1, 2:
+                    System.out.println("Время года - зима.");
+                    break;
+                case 3, 4, 5:
+                    System.out.println("Время года - весна.");
+                    break;
+                case 6, 7, 8:
+                    System.out.println("Время года - лето.");
+                    break;
+                case 9, 10, 11:
+                    System.out.println("Время года - осень.");
+                    break;
+                default:
+                    System.out.println("Такого месяца не существует.");
+            }
         }
     }
-}
